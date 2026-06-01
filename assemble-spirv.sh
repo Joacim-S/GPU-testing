cf3656d1e16d20595fec57dad534d8791ef71c24
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2026-06-01 17:37:39 +0300

    Add instructions to script

--- assemble-spirv.sh
+++ assemble-spirv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Assembles the spvasm files. Requires spirv-tools.
 mkdir spirv/spv
 cp -r spirv/spvasm/* spirv/spv/
 find spirv/spv -type f -exec spirv-as {} -o {} \; -exec rename .spvasm .spv {} \;

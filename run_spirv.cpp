b5622de5ecf5288d12cbf45b6171cb2d8cf23d24
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2026-06-03 19:10:54 +0300

    Work on parallel kernels

--- run_spirv.cpp
+++ run_spirv.cpp
@@ -1,3 +1,4 @@
+#include <ostream>
 #define CL_TARGET_OPENCL_VERSION 300
 #include<bits/stdc++.h>
 #include "fileutils.h"
@@ -73,6 +74,7 @@ int main(int argc, char *argv[]) {
 
 
     //Initialize memory and set kernel args.
+    //TODO: Reserve one big memory area. This takes forever.
     std::vector<uint> constArgs_h;
     size_t arg_count = 0;
     for (int i=0; i<inputs.size(); i++) {
@@ -102,31 +104,34 @@ int main(int argc, char *argv[]) {
         }
     }
     
-    cl_queue_properties properties[] = { CL_QUEUE_PROPERTIES, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, 0 };
-    cl_command_queue q = clCreateCommandQueueWithProperties(context, device_id, properties, &ret);
+    cl_queue_properties properties[] = { CL_QUEUE_PROPERTIES, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE | CL_QUEUE_PROFILING_ENABLE, 0 };
+    cl_command_queue *queues = new cl_command_queue[kernel_count]; //Need several queus to get parallel execution. 1k works, 10k is too much.
+    for (int i = 0; i < kernel_count; ++i) {
+        queues[i] = clCreateCommandQueueWithProperties(context, device_id, properties, &ret);
+    }
     check(ret, "q");
 
-    //Command buffers?
     cl_event user_event = clCreateUserEvent(context, &ret);
+    cl_event* kernel_events = new cl_event[kernel_count];
     check(ret, "Create event");
     for (int k=0; k<kernel_count; k++) {
-        ret = clEnqueueNDRangeKernel(q, kernels[k], 1, NULL, wgsize, wlsize, 1, &user_event, NULL);
+        ret = clEnqueueNDRangeKernel(queues[k], kernels[k], 1, NULL, wgsize, wlsize, 1, &user_event, &kernel_events[k]);
         check(ret, "Enqueue");
     }
     std::cout << "Enqueued" << std::endl;
     clSetUserEventStatus(user_event, CL_COMPLETE);
-    check(clFinish(q));
+    std::cout << "Finished" << std::endl;
     for (int k=0; k<kernel_count; k++) {
         int ko = k * arg_count;
-        std::cout << OUT << output << std::endl;
-        std::cout << "Result:" << std::endl;
+        std::cout << OUT << output << '\n';
+        std::cout << "Result:" << '\n';
         for (int i=0; i<arg_count; i++) {
-            check(clEnqueueReadBuffer(q, args_d[i + ko], true, 0, sizeof(uint) * args_h[i + ko].size(), args_h[i + ko].data(), 0, NULL, NULL), "read");
+            check(clEnqueueReadBuffer(queues[k], args_d[i + ko], true, 0, sizeof(uint) * args_h[i + ko].size(), args_h[i + ko].data(), 0, NULL, NULL), "read");
             std::cout << arg_names[i] << ": ";
             for (int j=0; j<args_h[i + ko].size(); j++) {
                 std::cout << args_h[i + ko][j] << " ";
             }
-            std::cout << std::endl;
+            std::cout << '\n';
         }
     }
 
@@ -135,13 +140,15 @@ int main(int argc, char *argv[]) {
         check(clReleaseKernel(kernels[i]), "kernelrelease");
     }
     check(clReleaseProgram(program));
-    check(clReleaseCommandQueue(q));
+    for (int i=0; i<kernel_count; i++) check(clReleaseCommandQueue(queues[i]));
     check(clReleaseContext(context));
 
     delete[] il;
     delete[] pname;
     delete[] output;
     delete [] kernels;
+    delete [] kernel_events;
+    delete [] queues;
     for (int i=0; i<arg_names.size(); i++) delete[] arg_names[i];
     for (int i=0; i<inputs.size(); i++) delete[] inputs[i];
 }

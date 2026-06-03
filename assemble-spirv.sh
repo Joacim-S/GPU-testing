2b16aef0d2a6041c01035c2fe7e80152306f187a
Sarén Joacim S <joqsaren@lx1-500-008.ad.helsinki.fi>
2026-06-03 10:33:46 +0300

    Improve assembler script

--- assemble-spirv.sh
+++ assemble-spirv.sh
@@ -2,4 +2,10 @@
 # Assembles the spvasm files. Requires spirv-tools.
 mkdir spirv/spv
 cp -r spirv/spvasm/* spirv/spv/
-find spirv/spv -type f -name '*.spvasm' -exec spirv-as '{}' -o '{}' \; -exec rename .spvasm .spv '{}' \;
+
+find spirv/spv -type f -name '*.spvasm' -exec bash -c '
+  for file; do
+    # Compile directly to the .spv extension, then remove the source .spvasm copy
+    spirv-as "$file" -o "${file%.spvasm}.spv" && rm "$file"
+  done
+' _ {} +

a95fe6f06bc0412ebbf2715fab814e39f8536bbe
Sarén Joacim S <joqsaren@lx1-500-008.ad.helsinki.fi>
2026-06-25 13:41:52 +0300

    Sketch parser

--- parser.py
+++ parser.py
@@ -6,3 +6,59 @@
 # Have x[id] r0[id] and so on
 # Else == prev(x) + 1 = max(x)?
 
+import argparse
+
+class KernelArg():
+    def __init__(self, glob: bool, typ: str, name: str):
+        self.glob = glob
+        self.type = typ
+        self.name = name
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    parser.add_argument('-o', '--output')
+    args = parser.parse_args()
+    return args
+
+def main():
+    output = """
+    static void do_stress(__global uint* scratchpad, uint group_id, __global uint* locations, uint iterations) {
+        for (uint i=0; i<iterations; i++) {
+            uint tmp1 = scratchpad[locations[group_id]];
+            if (tmp1 > 100) {
+                scratchpad[locations[group_id]] = i;
+                break;
+            }
+            scratchpad[locations[group_id]] = tmp1 + 1;
+            uint tmp2 = scratchpad[locations[group_id]];
+            if (tmp2 > 100) {
+                scratchpad[locations[group_id]] = i;
+                break;
+            }
+        }
+    }
+
+    """
+    args = parse_args()
+    arg_objs = set()
+    with open(args.filename, 'r') as f:
+        for row in f:
+            r = row.strip()
+            if r.startswith('__kernel'):
+                output += row
+                i = 0
+                while r[i] != '(':
+                    i += 1
+                i += 1
+                kernel_args = row[i:row.find(')')].split(', ')
+                for a in kernel_args:
+                    arg = a.split(' ')
+                    if arg[0] == 'global':
+                        arg_obj = KernelArg(True, arg[1], arg[2])
+                        arg_objs.add(arg_obj)
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
